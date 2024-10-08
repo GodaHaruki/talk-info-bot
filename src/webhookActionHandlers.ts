fabd0e005df2ec85d3d6972986f6b7562eef4496
GodaHaruki <haru.11.saki.11@gmail.com>
2024-10-09 05:01:43 +0900

    fix easier to read

--- src/webhookActionHandlers.ts
+++ src/webhookActionHandlers.ts
@@ -4,13 +4,13 @@ import { reply } from "./line";
 export function onJoinEvent(evt: JoinEvent) {
     reply(evt.replyToken, [{
         type: "text",
-        text: `${JSON.stringify(evt.source)}`
+        text: JSON.stringify(evt.source)
     }]);
 }
 
 export function onFollowEvent(evt: FollowEvent) {
     reply(evt.replyToken, [{
         type: "text",
-        text: `${JSON.stringify(evt.source)}`
+        text: JSON.stringify(evt.source)
     }]);
 }
